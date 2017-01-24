7a484b8f793cf7dc813e3589a37fe3a27691282a
ying <wyingtian@gmail.com>
2017-01-24 17:10:24 -0500

    finished most part

--- Assignment1/Assignment1/InstrumentationViewController.swift
+++ Assignment1/Assignment1/InstrumentationViewController.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class InstrumentationViewController: UIViewController {
 
+    @IBOutlet weak var Section1: UIView!
+    @IBOutlet weak var Section2: UIView!
+    @IBOutlet weak var Slider1: UISlider!
+    @IBOutlet weak var Slider2: UISlider!
     override func viewDidLoad() {
+        Section1.layer.cornerRadius = 10
+        Section2.layer.cornerRadius = 10
+        Slider1.transform = CGAffineTransform(rotationAngle: -(CGFloat)(M_PI_2))
+        Slider2.transform = CGAffineTransform(rotationAngle: -(CGFloat)(M_PI_2))
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
