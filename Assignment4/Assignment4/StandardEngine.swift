1d313fb54ae37d8b0cf5bcc77679100543ceafbf
ying <wyingtian@gmail.com>
2017-04-17 17:19:19 -0400

    finished asn4

---
+++ Assignment4/Assignment4/StandardEngine.swift
@@ -0,0 +1,9 @@
+//
+//  StandardEngine.swift
+//  Assignment4
+//
+//  Created by Ying on 4/13/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import Foundation
